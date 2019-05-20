ecba36076f6c31b21246114e3ff9e85afe42717f
Kon Kri <kon.kri@outlook.com>
2019-05-20 18:04:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,104 @@
+$( document ).ready(function() {
+    console.log( "Hello little one! Tony Stark dies" );
+        
+
+    $(".navbar").on('show.bs.collapse hide.bs.collapse', function(){
+        $("#toggleHumburgerBtn").toggleClass("is-active");
+    });
+
+
+    $(window).scroll(function() {
+        var $height = $(window).scrollTop();
+        if($height > 50) {
+            $('.scroll-variable-navbar').removeClass('scroll-variable-navbar-hidden');
+        }
+        else {
+            $('.scroll-variable-navbar').addClass('scroll-variable-navbar-hidden');
+        }
+    });
+
+    // MapBox
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia29ua3JpIiwiYSI6ImNqdnY2eWd1NjNzZHA0OXBic2Q2aXhoMDgifQ.zF2_d5xcqvmLJ190Qg6_8w';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center:[23.8745281,35.4891808],
+    zoom: 10
+    });
+
+    var size = 100;
+
+    var pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+         
+        onAdd: function() {
+            var canvas = document.createElement('canvas');
+            canvas.width = this.width;
+            canvas.height = this.height;
+            this.context = canvas.getContext('2d');
+        },
+         
+        render: function() {
+            var duration = 1000;
+            var t = (performance.now() % duration) / duration;
+            
+            var radius = size / 2 * 0.3;
+            var outerRadius = size / 2 * 0.7 * t + radius;
+            var context = this.context;
+            
+            // draw outer circle
+            context.clearRect(0, 0, this.width, this.height);
+            context.beginPath();
+            context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+            context.fillStyle = 'rgba(105, 170, 6,' + (1 - t) + ')';
+            context.fill();
+            
+            // draw inner circle
+            context.beginPath();
+            context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+            context.fillStyle = 'rgba(44, 100, 1, 1)';
+            context.strokeStyle = 'white';
+            context.lineWidth = 2 + 4 * (1 - t);
+            context.fill();
+            context.stroke();
+            
+            // update this image's data with data from the canvas
+            this.data = context.getImageData(0, 0, this.width, this.height).data;
+            
+            // keep the map repainting
+            map.triggerRepaint();
+            
+            // return `true` to let the map know that the image was updated
+            return true;
+        }
+    };
+         
+    map.on('load', function () {
+        
+        map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+        
+        map.addLayer({
+            "id": "points",
+            "type": "symbol",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [23.7826728,35.467349]
+                        }
+                    }]
+                }
+            },
+            "layout": {
+                "icon-image": "pulsing-dot"
+            }
+        });
+    });
+    
+});
