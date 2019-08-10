7eb81994aa32bb3f6fd287375795f4b1acfc1028
Dimitris Grevenos <dimgrev007@gmail.com>
2019-08-10 04:13:03 +0300

    minor fixes and lazy load custom added

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ $( document ).ready(function() {
     });
 
     $('.imageGallery').slick({
-        lazyLoad: 'ondemand',
+        // lazyLoad: 'ondemand', //not needed anymore...
         prevArrow:'<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
         nextArrow:'<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
     });
